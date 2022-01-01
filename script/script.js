0123e708e31f5a8b10cd63330ec193f8d961ebff
auung <82633351+auung@users.noreply.github.com>
2022-01-01 20:15:10 +0630

    deleted hard data and added AJAX

--- script/script.js
+++ script/script.js
@@ -19,29 +19,36 @@ function addActive(element) {
 			timelapse_p[i].classList.remove("active");
 		};
 	};
-	changeData(element.dataset.time);
+	changeData(element.innerHTML);
 };
 
+let request = new XMLHttpRequest();
+let data;
+request.open("GET", "https://auung.github.io/auung-json/Data/data.json");
+request.onload = function() {
+	data = JSON.parse(request.responseText);
+}
+request.send();
+
+let timeSpent_p = document.getElementsByClassName("time-spent");
+let previousTimeSpent_p = document.getElementsByClassName("previous-time-spent");
+let lastTime_span = document.getElementsByClassName("last-time");
+
 function changeData(time) {
-	let timeSpent_p = document.getElementsByClassName("time-spent");
-	let lastTime_span = document.getElementsByClassName("last-time");
-	let previousTime_span = document.getElementsByClassName("previous-time-spent");
-
-	for (let i = 0; i < timeSpent_p.length; i++) {
-		timeSpent_p[i].innerHTML = timeSpent_p[i].dataset[time];
-	}
-
-	for (let i = 0; i < lastTime_span.length; i++) {
-		let timeDisplay = "";
-		if (time == "day") {
-			timeDisplay = "yesterday";
-		} else {
-			timeDisplay = "last " + time;
-		}
-		lastTime_span[i].innerHTML = timeDisplay;
-	}
-
-	for (let i = 0; i < previousTime_span.length; i++) {
-		previousTime_span[i].innerHTML = previousTime_span[i].dataset[time];
-	}
+	time = time.toLowerCase();
+	for (let i = 0; i < 6; i++) {
+		timeSpent_p[i].innerHTML = data[i].timeframes[time].current + "hrs";
+		previousTimeSpent_p[i].innerHTML = data[i].timeframes[time].previous + "hrs";
+		switch(time) {
+			case "daily":
+				lastTime_span[i].innerHTML = "Yesterday";
+				break;
+			case "weekly":
+				lastTime_span[i].innerHTML = "Last Week";
+				break;
+			case "monthly":
+				lastTime_span[i].innerHTML = "Last Month";
+				break;
+		};
+	};
 };
